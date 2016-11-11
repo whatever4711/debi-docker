134c174fa7fb1a65316fb805933db5d787de21bc
Marcel Großmann <marcel.grossmann@uni-bamberg.de>
2016-11-11 20:59:53 +0100

    Clean Up

--- armbian/initialize.sh
+++ armbian/initialize.sh
@@ -2,15 +2,13 @@
 
 DIR=/armbian
 
-gpg --ignore-time-conflict --no-options --no-default-keyring --homedir /tmp/tmp.JrQS7d54Sa --no-auto-check-trustdb --trust-model always --keyring /etc/apt/trusted.gpg --primary-keyring /etc/apt/trusted.gpg --keyserver http://keys.gnupg.net --recv-keys E083A3782A194991
-echo "deb http://repo.aptly.info/ squeeze main" > /etc/apt/sources.list.d/aptly.list
-apt-get update && apt-get install -y --force-yes git aptly
+apt-get update && apt-get install -y --force-yes git 
 
 mkdir $DIR
-cd $DIR && git clone https://github.com/igorpecovnik/lib.git --depth 1
+#cd $DIR && git clone https://github.com/igorpecovnik/lib.git --depth 1
 
-cp $DIR/lib/compile.sh $DIR/.
-chmod +x $DIR/compile.sh
+#cp $DIR/lib/compile.sh $DIR/.
+#chmod +x $DIR/compile.sh
 
 cat << EOF > lamobo.sh
 #!/bin/bash
@@ -19,5 +17,5 @@ EOF
 
 chmod +x lamobo.sh
 
-./compile.sh BRANCH=next BOARD=lamobo-r1 KERNEL_ONLY=yes KERNEL_CONFIGURE=no BUILD_DESKTOP=no PROGRESS_DISPLAY=plain RELEASE=jessie
+#./compile.sh BRANCH=next BOARD=lamobo-r1 KERNEL_ONLY=yes KERNEL_CONFIGURE=no BUILD_DESKTOP=no PROGRESS_DISPLAY=plain RELEASE=jessie
 
